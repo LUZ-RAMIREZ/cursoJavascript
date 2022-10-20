7a3c3ddb87d29cecb1cbab423887c85d766d3164
Aura <betty.ramirezrza@gmail.com>
2022-10-20 10:35:19 -0500

    trabajo de operadores

---
+++ 3.Operadores/ejercicios.js
@@ -0,0 +1,20 @@
+///aritmetico//
+let numero=(2+5+6);
+console.log(numero);
+
+
+//comparacion//
+let num1=8
+let num2=8
+console.log(num1==num2)
+
+//logica//
+var cierto=true
+var falso = !cierto
+console.log(5>4)
+console.log(!(5>4))
+
+//asignacion//
+let nume=5
+nume = nume + 7
+console.log(nume)
--- 3.Operadores/index.js
+++ 3.Operadores/index.js
@@ -1,28 +1,27 @@
 //liszalobo14@gmail.com
 //--------------------------------------------------------------------------------------------------------//
 //OPERADORES
-//Los operadores permiten manipular las operaciones variables, realizar matemáticas, 
-//comparaciones lógicas o asignaciones.
+/*Los operadores permiten manipular las operaciones variables, realizar matemáticas, 
+  comparaciones lógicas o asignaciones.*/
 //--------------------------------------------------------------------------------------------------------//
 //TIPOS DE OPERDPORES
 //1.OPERADORES DE ASIGNACIÓN
-//BRENY - el operdore de asignacion (=) asigna un valor a su operando izquierdo basandose en el valor de su
-// operando derecho.
-//LUZ - Los operadores de asignación devuelven el valor del objeto especificado 
-//por el operando izquierdo después de la asignación(=)
-//2.OPERADORES BINARIOS
-//BRENY -
-//LUZ -
-//3.OPERADORES DE COMPARCION
-//BRENY - Ese operador comprueba si sus dos operandos, son iguales y le da un resultado booleano.
-//LUZ - Se encarga de coparar dos expresiones y devuelve un valor booleano(True, False o Null.) 
-//que representa la relacion de sus valores.
-//4.OPERADORES DE LOGICA
-//BRENY - Son operadores para trabajar con datos booleanos y logicos
-//LUZ - Se usan para combinar dos valores Booleanos y devolver un resultado verdadero, falso o nulo
-//5.OPERADORES ARITMÉTICOS
-//BRENY - Con ellos se pueden realizar operaciones y calculos matematicos.
-//LUZ - Se usan para calcular un valor de dos a más o cambiar 
-//su signo de un numero de positivo a negativo o vicibersa
+  /*BRENY - el operdore de asignacion (=) asigna un valor a su operando izquierdo basandose en el valor de su
+    operando derecho.*/
+  /*LUZ - Los operadores de asignación devuelven el valor del objeto especificado
+    por el operando izquierdo después de la asignación(=)*/
+//2.OPERADORES DE COMPARCION
+  /*BRENY - Ese operador comprueba si sus dos operandos, son iguales y le da un resultado booleano.
+  /*LUZ - Se encarga de coparar dos expresiones y devuelve un valor booleano(True, False o Null.) 
+    que representa la relacion de sus valores.*/
+//3.OPERADORES DE LOGICA
+  /*BRENY - Son operadores para trabajar con datos booleanos y logicos*/
+  /*LUZ - Se usan para combinar dos valores Booleanos y devolver un resultado verdadero, falso o nulo*/
+//4.OPERADORES ARITMÉTICOS
+  /*BRENY - Con ellos se pueden realizar operaciones y calculos matematicos.*/
+  /*LUZ - Se usan para calcular un valor de dos a más o cambiar 
+  su signo de un numero de positivo a negativo o vicibersa*/
 //--------------------------------------------------------------------------------------------------------//
 //CONCLUSION
+/* Los operadores permiten a los programas realizar cálculos complejos 
+ y tomar decisiones lógicas en función de comparaciones y otros tipos de condiciones.*/
